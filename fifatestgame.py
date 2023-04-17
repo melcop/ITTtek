2a951c154633c82f866bd69449af5753ced74a5f
MelCop <charliedem20@gmail.com>
2023-04-17 16:47:21 +0200

    videre med fifagame

--- fifatestgame.py
+++ fifatestgame.py
@@ -1,2 +1,12 @@
-semifinals = ["Argentina", "Brasil", "France", "Germany"]
+import random
 
+# Vi har en list med 4 lande
+semifinals = ["Argentina", "Brasil", "France", "Germany"]
+# Her har vi længden på listen
+sizeofsemifinals = len(semifinals)
+#print(sizeofsemifinals)
+
+# Her skal vi matche landene med hinanden, til to kampe(a mod b og c mod d)
+for i in semifinals:
+    a = random.randrange(0, sizeofsemifinals)
+    print(a)
