c171c41d9d1c1d8e7915cd04ad7e7597e803a6de
MelCop <charliedem20@gmail.com>
2023-04-30 19:47:17 +0200

    Student klasse

---
+++ Student.py
@@ -0,0 +1,36 @@
+class Student:
+    """A Python class to store student information"""
+
+    def __init__(self, name, address, age):
+        self.name = name
+        self.address = address
+        self.age = age
+        
+    def return_name(self):
+        """return student name"""
+        return self.name
+
+    def return_age(self):
+        """return student age"""
+        return self.age
+
+    def return_address(self):
+        """return student address"""
+        return self.address
+    
+    def update_address(self, address):
+        """update student address"""
+        self.address = address
+        return self.address
+        
+student1 = Student("John Doe", "Lygten 16, 2400, NV", "29")
+
+print(student1.name)
+print(student1.age)
+print(student1.address)
+
+student2 = Student("Jane Doe", "123 Main Street, San Jose, CA", "27")
+print(student1.address())
+print(student1.update_address("234 Main Street, Newark, CA"))
+Student(student1, "John Doe", "29", "123 Main Street, Newark, CA")
+Student.return_address(student1)
