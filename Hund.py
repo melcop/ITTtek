adb46829d62af9640aa528f6e494f954f308f3e1
MelCop <charliedem20@gmail.com>
2023-05-01 11:23:07 +0200

    hundeklasse

---
+++ Hund.py
@@ -0,0 +1,13 @@
+class Hund:
+    def __init__(self, navn, alder):
+        self.navn = navn
+        self.alder = alder
+
+    def bark(self):
+        print("Vov!")
+
+# Opret en instans af klassen Hund
+min_hund = Hund("Fido", 3)
+
+# Brug metoden bark på instansen
+min_hund.bark()
