4496a608b7ffaa2c958a63061938c8620086cb1b
MelCop <charliedem20@gmail.com>
2023-05-21 19:39:26 +0200

    fifachanges

--- Exercises/FIFA/fifatestgame.py
+++ Exercises/FIFA/fifatestgame.py
@@ -9,7 +9,11 @@ sizeofsemifinals = len(semifinals)
 # Este forloop es algoritmo (no está terminado) para elegir que paises se enfrentan
 for i in semifinals:
     a = random.randrange(0, sizeofsemifinals)
-    print(i)
+    b = random.randrange(0, sizeofsemifinals)
+    #print(a, "vs", b)
+    c = semifinals[0]
+    d = semifinals[1]
+    print(c, a,"vs", b ,d)
     
     # No está terminado, tengo que borrar el país elegido para que quede uno menos en la lista
     #semifinals.remove
