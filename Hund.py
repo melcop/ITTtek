beef90d0fbc66dd6ce548a9bf6f457962a28968b
MelCop <charliedem20@gmail.com>
2023-05-25 11:46:01 +0200

    hund

--- Hund.py
+++ Hund.py
@@ -5,9 +5,14 @@ class Hund:
 
     def bark(self):
         print("Vov!")
+        
+    def happy(self):
+        print("O-O")
 
 # Opret en instans af klassen Hund
 min_hund = Hund("Fido", 3)
 
 # Brug metoden bark på instansen
 min_hund.bark()
+# Glad hund
+min_hund.happy()
