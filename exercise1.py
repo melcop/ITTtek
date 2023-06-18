c0f0903261685e85636c35bf903198656e898bbb
MelCop <charliedem20@gmail.com>
2023-06-18 16:26:50 +0200

    changes

---
+++ exercise1.py
@@ -0,0 +1,12 @@
+a=1
+b=2
+c=3
+
+if a>b and a>c:
+    print("a er det højest nummer")
+elif b>a and b>c:
+    print("b er det højest nummer")
+elif c>a and c>b:
+    print("c er det højest nummer")
+else:
+    print("Alle tre er lige høje")
