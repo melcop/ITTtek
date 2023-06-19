f067c4d85ea28d0c2ac4f64d5d10b1610ace9aca
MelCop <charliedem20@gmail.com>
2023-06-19 07:54:52 +0200

    changes

--- exercise1.py
+++ exercise1.py
@@ -8,5 +8,7 @@ elif b>a and b>c:
     print("b er det højest nummer")
 elif c>a and c>b:
     print("c er det højest nummer")
+elif a==b and a>c:
+    print("a og b er lige, mens a er større end c")
 else:
     print("Alle tre er lige høje")
