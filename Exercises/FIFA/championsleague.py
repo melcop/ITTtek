d74b55defb6fe9641607697d0ad08e6e7180a12c
melcop <charliedem20@gmail.com>
2023-07-07 21:32:09 +0200

    ChampionsLeague

---
+++ Exercises/FIFA/championsleague.py
@@ -0,0 +1,6 @@
+import random
+team = " "
+pot_a = ["Manchester City", "FC Barcelona","Napoli", "Bayern Munich", "Sevilla", "PSG", "Benfica", "Feyernood"]
+pot_b = ["Real Madrid", "Manchester United", "Inter de Milan", "Borussia Dortmund", "Atletico de Madrid", "Leipzig", "Porto", "Arsenal"]
+pot_c = ["Shaktar Donetsk", "Salzburg", "Milan", "Lazio", "Red Star",]
+pot_d = ["Berlin Union", "Lens"]
