fd483d4d525481d840f7bfe1d406c3d5ed27c4aa
melcop <charliedem20@gmail.com>
2023-07-08 15:20:44 +0200

    champions

--- Exercises/FIFA/championsleague.py
+++ Exercises/FIFA/championsleague.py
@@ -4,3 +4,17 @@ pot_a = ["Manchester City", "FC Barcelona","Napoli", "Bayern Munich", "Sevilla",
 pot_b = ["Real Madrid", "Manchester United", "Inter de Milan", "Borussia Dortmund", "Atletico de Madrid", "Leipzig", "Porto", "Arsenal"]
 pot_c = ["Shaktar Donetsk", "Salzburg", "Milan", "Lazio", "Red Star",]
 pot_d = ["Berlin Union", "Lens"]
+
+group_a =[]
+group_b =[]
+group_c =[]
+group_d =[]
+group_e =[]
+group_f =[]
+group_g =[]
+group_h =[]
+
+
+print(pot_a)
+
+# function for random group maker
