242fab39d998afe6efc63d596218cc5bed1a0d75
melcop <charliedem20@gmail.com>
2023-07-17 20:32:07 +0200

    changes

--- countingstars.py
+++ countingstars.py
@@ -1,6 +1,11 @@
-print("Insert a number, prick!: ")
-q = input()
-p= int(q)
+import random, time
+
+p = random.randint(1,1000)
+print(p)
+time.sleep(10)
+
+#q = input()
+#p = int(q)
 for x in range(p):
     for y in range(p):
         for z in range(p):
