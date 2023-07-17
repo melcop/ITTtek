add8e86195a875663621bbc275fe9ad23422810f
melcop <charliedem20@gmail.com>
2023-07-18 01:33:03 +0200

    changes

--- countingstars.py
+++ countingstars.py
@@ -3,7 +3,9 @@ import random, time
 p = random.randint(1,1000)
 print(p)
 time.sleep(10)
-
+x = random.randint(25,100)
+y = random.randint(30,110)
+z = random.randint(40,120)
 #q = input()
 #p = int(q)
 for x in range(p):
@@ -11,4 +13,5 @@ for x in range(p):
         for z in range(p):
             print(x, y, z)
 
-print("Try with threads")
+
+#print("Try with threads")
