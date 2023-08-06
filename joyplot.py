2d63f012f357e7ff910934c8ab12947e8a68dcab
MelCop <charliedem20@gmail.com>
2023-08-06 11:39:13 +0200

    changes

--- joyplot.py
+++ joyplot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 # Lav et dataset af random numre
-data = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [6, 7, 8, 9, 10]})
+data = pd.DataFrame({'A': [1, 2, 3, 4, 5, 6], 'B': [7, 8, 9, 10, 11, 12]})
 
 # Lav en joy plot
 fig, axes = joypy.joyplot(data, ylim='own')
