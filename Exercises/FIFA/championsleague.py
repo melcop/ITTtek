5f97f0fc6a6965af657514b5826a3ce03a2356f4
MelCop <charliedem20@gmail.com>
2023-10-04 17:05:16 +0200

    changes

--- Exercises/FIFA/championsleague.py
+++ Exercises/FIFA/championsleague.py
@@ -2,7 +2,7 @@ import random
 team = " "
 pot_a = ["Manchester City", "FC Barcelona","Napoli", "Bayern Munich", "Sevilla", "PSG", "Benfica", "Feyernood"]
 pot_b = ["Real Madrid", "Manchester United", "Inter de Milan", "Borussia Dortmund", "Atletico de Madrid", "Leipzig", "Porto", "Arsenal"]
-pot_c = ["Shaktar Donetsk", "Salzburg", "Milan", "Lazio", "Red Star", "Real Sociedad", "Celtic", "Newcastle"]
+pot_c = ["Shaktar Donetsk", "Salzburg", "Milan", "Lazio", "Red Star", "Real Sociedad", "Celtic", "Newcastle", "Copenhagen"]
 pot_d = ["Berlin Union", "Lens"]
 
 group_a =[]
