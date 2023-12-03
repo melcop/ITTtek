e2cb9af6d15e0be2f4dd2659121b7aae2a9ddc49
MelCop <charliedem20@gmail.com>
2023-12-03 10:32:21 +0100

    hvilkendag

---
+++ hvilkendag.py
@@ -0,0 +1,13 @@
+dag = 3
+if dag == 1:
+    print("Mandag")
+elif dag == 2:
+    print("Tirsdag")
+elif dag == 3:
+    print("Onsdag")
+elif dag == 4:
+    print("Torsdag")
+elif dag == 5:
+    print("Fredag")
+else:
+    print("Weekend")
