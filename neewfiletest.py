540dfc02d9f4bcab4813537bee98d57bc9318ecf
MelCop <charliedem20@gmail.com>
2023-12-08 11:15:13 +0100

    test 3 interesting that x=10 a not 30  print hello

--- neewfiletest.py
+++ neewfiletest.py
@@ -3,3 +3,13 @@ num3 = [3, 4, 5]
 length = len(nums)
 print(nums > num3)
 print(length)
+
+# Test 2 - Dictionary
+x = {1: "a", 2: "b"}
+y = x.keys()
+print(y)
+
+# Test 3 
+a = 10
+if a == 30 or 40 or 60 :
+    print('Hello')
