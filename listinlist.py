37a3bf2f2a69980d00237d69223a427a6a70db62
MelCop <charliedem20@gmail.com>
2023-12-10 14:03:43 +0100

    range

--- listinlist.py
+++ listinlist.py
@@ -4,3 +4,13 @@ print(data[2])
 print(" ---------------- ")
 data[0] = 100
 print(data)
+
+# For loop
+for num in range(5):
+    print("Value:{}".format(num))
+
+
+print("----------------------------------")
+# Start, stop, og step for range funktionen
+for num in range(2, 10, 2):
+    print("Value:{}".format(num))
