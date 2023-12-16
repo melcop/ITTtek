d7c414baf99394d60ed5ac06a73d36c37b50d548
melcop <charliedem20@gmail.com>
2023-12-16 12:41:05 +0100

    prime number

--- listinlist.py
+++ listinlist.py
@@ -34,3 +34,31 @@ print("----------------------------------")
 lis = [[8, 7], [6,5]]
 result = [p + q for p, q in lis]
 print(result)
+
+print("----------------------------------")
+# Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
