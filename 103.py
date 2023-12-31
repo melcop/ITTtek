9a73c353b5bb6990278b94ef0d911a67ac3deb72
MelCop <charliedem20@gmail.com>
2023-12-31 13:45:10 +0100

    rangeograndom

--- 103.py
+++ 103.py
@@ -1,3 +1,4 @@
+import random
 print(min(max(False, -3, -4), 2, 7))
 
 print("----------------------------")
@@ -7,3 +8,9 @@ for n in range(21, 40):
     if n % 2 ==0:
         a.add(n)
 print(a)
+print("----------------------------")
+
+
+for _ in range(5):  # Skifter "5" ud med det ønskede antal tilfældige tal
+    random_number = random.randint(1, 100)
+    print(random_number)
