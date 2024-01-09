01b29cc92b557a201d9401e707cbaa0fdeda8a3f
melcop <charliedem20@gmail.com>
2024-01-09 19:34:18 +0100

    primenumber

--- 103.py
+++ 103.py
@@ -45,3 +45,32 @@ dict_b = {"bread": 2.20, "jam": 4.87}
 dict_c = {}
 
 print(len(dict_a))
+
+
+print("----------------------------") 
+# Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
