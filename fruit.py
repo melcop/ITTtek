02c7a8bddbc7aee6f7145c3b6285174f09492e75
melcop <charliedem20@gmail.com>
2024-01-18 20:30:48 +0100

    fruit

---
+++ fruit.py
@@ -0,0 +1,10 @@
+import random
+
+# Opret en liste med 10 forskellige frugter
+frugter = ["æble", "banan", "appelsin", "jordbær", "kiwi", "ananas", "mango", "drue", "pære", "fersken"]
+
+# Vælg en tilfældig frugt fra listen
+tilfældig_frugt = random.choice(frugter)
+
+# Udskriv den valgte frugt
+print("Den tilfældigt valgte frugt er:", tilfældig_frugt)
