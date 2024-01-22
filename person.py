fe85c02cf02f50d0b268dcead26ffa71f74076ad
melcop <charliedem20@gmail.com>
2024-01-22 17:34:41 +0100

    Changes

--- person.py
+++ person.py
@@ -1,9 +1,10 @@
 class Person:
     '''This class represents a person object'''
 
-    def __init__(self, name, tel):
-        self.name = name
+    def __init__(self, first_name, surname, tel):
+        self.first_name = first_name
+        self.surname = surname
         self.tel = tel
 
-p = Person("Simon", "12344567")
-print(p.name)
+p = Person("Simon", "Nielsen", "12344567")
+print(p.first_name)
