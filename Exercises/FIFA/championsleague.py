ce7ef56b779a4108c091ead5376cbef091d93168
MelCop <charliedem20@gmail.com>
2024-02-15 09:30:01 +0100

    changes

--- Exercises/FIFA/championsleague.py
+++ Exercises/FIFA/championsleague.py
@@ -23,21 +23,34 @@ pos = 0
 #Insert teams to group_list
 # using insert() + loop
 # to insert one list in another
-for i in range(len(pot_a)):
-    group_list.insert(i + pos, pot_a[i])
+#for i in range(len(pot_a)):
+#    group_list.insert(i + pos, pot_a[i])
 
-print(group_list)
+#print(group_list)
+
+# Bland elementerne i liste A tilfældigt
+random.shuffle(pot_a)
+
+# Fordele elementerne fra A tilfældigt på de 8 lister i B
+for element in pot_a:
+    tilfældig_liste_index = random.randint(0, 7)
+    group_list[tilfældig_liste_index].append(element)
+
+# Vis resultatet
+for i, liste in enumerate(group_list):
+    print(f"Liste {i+1}: {liste}")
+
 
 #Insert teams to group_a
-for i in pot_a:
-    a = random.randrange(0, sizeofgroup)
-    b=pot_a[a]
-    group_a.append(b)
+#for i in pot_a:
+#    a = random.randrange(0, sizeofgroup)
+#    b=pot_a[a]
+#    group_a.append(b)
     #print(a)
 
-print("pot A: ", pot_a)
-print("pot B: ", pot_b)
-print("pot C: ", pot_c)
-print("pot D: ", pot_d)
-print(group_list)
+#print("pot A: ", pot_a)
+#print("pot B: ", pot_b)
+#print("pot C: ", pot_c)
+#print("pot D: ", pot_d)
+#print(group_list)
 # function for random group maker
