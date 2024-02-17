34bf402f8ddbe8f156a747d327989b74229a560f
MelCop <charliedem20@gmail.com>
2024-02-17 12:21:37 +0100

    sorteo de la champions

--- Exercises/FIFA/championsleague.py
+++ Exercises/FIFA/championsleague.py
@@ -2,19 +2,19 @@ import random
 team = " "
 pot_a = ["Manchester City", "FC Barcelona","Napoli", "Bayern Munich", "Sevilla", "PSG", "Benfica", "Feyernood"]
 pot_b = ["Real Madrid", "Manchester United", "Inter de Milan", "Borussia Dortmund", "Atletico de Madrid", "Leipzig", "Porto", "Arsenal"]
-pot_c = ["Shaktar Donetsk", "Salzburg", "Milan", "Lazio", "Red Star", "Real Sociedad", "Celtic", "Newcastle", "Copenhagen"]
+pot_c = ["Shaktar Donetsk", "Salzburg", "Milan", "Lazio", "Real Sociedad", "Celtic", "Newcastle", "Copenhagen"]
 pot_d = ["Union Berlin", "Lens", "PSV", "Antwerp", "Braga", "Galatasaray", "Young Boys", "Crvena zvezda"]
 
-group_a =[]
-group_b =[]
-group_c =[]
-group_d =[]
-group_e =[]
-group_f =[]
-group_g =[]
-group_h =[]
+group_1 =[]
+group_2 =[]
+group_3 =[]
+group_4 =[]
+group_5 =[]
+group_6 =[]
+group_7 =[]
+group_8 =[]
 
-group_list = [group_a, group_b, group_c, group_d, group_e, group_f, group_g, group_h]
+group_list = [group_1, group_2, group_3, group_4, group_5, group_6, group_7, group_8]
 
 sizeofgroup = len(pot_a)
                   
@@ -32,13 +32,33 @@ pos = 0
 random.shuffle(pot_a)
 
 # Fordele elementerne fra A tilfældigt på de 8 lister i B
-for element in pot_a:
-    tilfældig_liste_index = random.randint(0, 7)
-    group_list[tilfældig_liste_index].append(element)
+#for element in pot_a:
+#    tilfældig_liste_index = random.randint(0, 7)
+#    group_list[tilfældig_liste_index].append(element)
+
+# Fordel elementerne fra A jævnt på de 8 lister i B
+for i, element in enumerate(pot_a):
+    liste_index = i % 8  # Brug modulus-operatoren for at få det jævne indeks
+    group_list[liste_index].append(element)
+
+# Fordel elementerne fra A jævnt på de 8 lister i B
+for i, element in enumerate(pot_b):
+    liste_index = i % 8  # Brug modulus-operatoren for at få det jævne indeks
+    group_list[liste_index].append(element)
+
+# Fordel elementerne fra A jævnt på de 8 lister i B
+for i, element in enumerate(pot_c):
+    liste_index = i % 8  # Brug modulus-operatoren for at få det jævne indeks
+    group_list[liste_index].append(element)
+
+# Fordel elementerne fra A jævnt på de 8 lister i B
+for i, element in enumerate(pot_d):
+    liste_index = i % 8  # Brug modulus-operatoren for at få det jævne indeks
+    group_list[liste_index].append(element)
 
 # Vis resultatet
 for i, liste in enumerate(group_list):
-    print(f"Liste {i+1}: {liste}")
+    print(f"Group {i+1}: {liste}")
 
 
 #Insert teams to group_a
