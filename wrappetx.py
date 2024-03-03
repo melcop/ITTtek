dbdb10609fc77df7d5b29ec4848d1aed82118a21
MelCop <charliedem20@gmail.com>
2024-03-03 18:10:18 +0100

    changes

--- wrappetx.py
+++ wrappetx.py
@@ -3,7 +3,7 @@ import textwrap
 text = "Python is useful, but C is powerful"
 
 # Wrap teksten to en specifik bredde
-wrapped_text = textwrap.wrap(text, width = 10)
+wrapped_text = textwrap.wrap(text, width = 12)
 
 # Print wrap tekst ud
 for line in wrapped_text:
