a250dc4addfcf309d5874f11a4f86344f131d076
MelCop <charliedem20@gmail.com>
2024-03-22 11:56:21 +0100

    rand

--- day152.py
+++ day152.py
@@ -1,4 +1,7 @@
+import random
 def some_func(a, b, c=0, d=1):
     return a + b + c + d
 result = some_func(1, 2, d=4)
-print(result)
+rand_int = random.randint(1, 100)
+total = result + rand_int
+print(total)
