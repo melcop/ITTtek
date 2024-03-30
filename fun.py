638d453b599d6560ffeabf57b372f1a72cb7ffdc
MelCop <charliedem20@gmail.com>
2024-03-30 11:43:57 +0100

    count

--- fun.py
+++ fun.py
@@ -3,6 +3,11 @@ dep = []
 dep.append(300)
 dep.append(200)
 dep.append('abe')
-print(dep)
-print("----------")
+
+
 print(dep.index("abe"))
+print("----------")
+dep.append('aaa')
+print(dep)
+
+print(dep.count(200))
