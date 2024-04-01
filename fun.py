f4ef14df9b2c8e3dbf174c47e8ae83c2f3c9863f
melcop <charliedem20@gmail.com>
2024-04-01 12:39:45 +0200

    c

--- fun.py
+++ fun.py
@@ -2,12 +2,12 @@ dep = []
 
 dep.append(300)
 dep.append(200)
-dep.append('abe')
+dep.append("abe")
 
 
 print(dep.index("abe"))
 print("----------")
-dep.append('aaa')
+dep.append("aaa")
 print(dep)
 
 print(dep.count(200))
@@ -17,7 +17,7 @@ dap = []
 dap.append(100)
 dap.append(150)
 dap.append("ccc")
-
+dap.insert(3, "abe")
 print("----------")
 print(dap)
 print("----------")
