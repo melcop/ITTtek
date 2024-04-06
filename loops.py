391ea3484c6a1cdfeb1ef5fc8393c4eb05363603
MelCop <charliedem20@gmail.com>
2024-04-06 14:40:22 +0200

    c

--- loops.py
+++ loops.py
@@ -3,5 +3,8 @@ tal = 10
 
 # Løkke der tæller ned med 1 og ganger med 3
 while tal > 0:
-    print(tal * 3)
+    total = tal * 3
+    print(total)
+    if total < 10:
+        print("Tjek, tal er mindre end 10")
     tal -= 1
