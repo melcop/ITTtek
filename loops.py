5a009fbbf3f33f8eebff04ea9c2976cb78c7d651
melcop <charliedem20@gmail.com>
2024-04-20 11:56:12 +0200

    infinite

--- loops.py
+++ loops.py
@@ -3,7 +3,7 @@ tal = 10
 
 # Løkke der tæller ned med 1 og ganger med 3
 while tal > 0:
-    total = tal * 3
+    total = tal * 3/7 
     print(total)
     if total < 10:
         print("Tjek, tal er mindre end 10")
