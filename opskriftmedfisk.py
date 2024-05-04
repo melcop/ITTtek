40b4ea7ac383e2fdfe2deb9f29dd8094e70f6edc
MelCop <charliedem20@gmail.com>
2024-05-04 12:25:37 +0200

    a

--- opskriftmedfisk.py
+++ opskriftmedfisk.py
@@ -1,28 +1,28 @@
 import random
 
-def generate_recipe(vegetables, meats, fishes, spices):
-    vegetable = random.choice(vegetables)
+def lav_opskrift(gronsag, kod, fisk, kryderier):
+    gronsag = random.choice(gronsag)
     if random.choice([True, False]):  # Vælg tilfældigt mellem fisk og kød
-        protein = random.choice(fishes)
+        protein = random.choice(fisk)
     else:
-        protein = random.choice(meats)
-    spice = random.choice(spices)
+        protein = random.choice(kod)
+    kryderier = random.choice(kryderier)
 
-    recipe = f"Opskrift: Stegt {protein} med {vegetable} og krydret med {spice}."
-    return recipe
+    opskrift = f"Opskrift: Stegt {protein} med {gronsag} og krydret med {kryderier}."
+    return opskrift
 
 # Liste med grøntsager
-vegetables = ["broccoli", "gulerødder", "spinat", "tomater", "aubergine", "peberfrugt"]
+gronsag = ["broccoli", "gulerødder", "spinat", "tomater", "aubergine", "peberfrugt"]
 
 # Liste med kød
-meats = ["kylling", "oksekød", "svinekød", "lam"]
+kod = ["kylling", "oksekød", "svinekød", "lam"]
 
 # Liste med fisk
-fishes = ["laks", "tun", "torsk", "rødspætte"]
+fisk = ["laks", "tun", "torsk", "rødspætte"]
 
 # Liste med krydderier
-spices = ["oregano", "timian", "rosmarin", "karry", "kanel", "spidskommen"]
+kryderier = ["oregano", "timian", "rosmarin", "karry", "kanel", "spidskommen"]
 
 # Generer og udskriv opskrift
-recipe = generate_recipe(vegetables, meats, fishes, spices)
-print(recipe)
+opskrift = lav_opskrift(gronsag, kod, fisk, kryderier)
+print(opskrift)
