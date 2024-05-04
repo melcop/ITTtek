40262f7af18317243321d08a6240900f7d63cf52
MelCop <charliedem20@gmail.com>
2024-05-04 12:12:14 +0200

    test

---
+++ opskrift.py
@@ -0,0 +1,22 @@
+import random
+
+def generate_recipe(vegetables, meats, spices):
+    vegetable = random.choice(vegetables)
+    meat = random.choice(meats)
+    spice = random.choice(spices)
+
+    recipe = f"Opskrift: Stegt {meat} med {vegetable} og krydret med {spice}."
+    return recipe
+
+# Liste med grøntsager
+vegetables = ["broccoli", "gulerødder", "spinat", "tomater", "aubergine", "peberfrugt"]
+
+# Liste med kød
+meats = ["kylling", "oksekød", "svinekød", "lam", "laks", "tun"]
+
+# Liste med krydderier
+spices = ["oregano", "timian", "rosmarin", "karry", "kanel", "spidskommen"]
+
+# Generer og udskriv opskrift
+recipe = generate_recipe(vegetables, meats, spices)
+print(recipe)
