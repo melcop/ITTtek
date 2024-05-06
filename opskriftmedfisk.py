1b24e3f47cf78a3396e968b06fc5a4dd3cc67116
MelCop <charliedem20@gmail.com>
2024-05-06 13:44:16 +0200

    a

--- opskriftmedfisk.py
+++ opskriftmedfisk.py
@@ -1,28 +1,36 @@
 import random
+import datetime
 
-def lav_opskrift(gronsag, kod, fisk, kryderier):
-    gronsag = random.choice(gronsag)
-    if random.choice([True, False]):  # Vælg tilfældigt mellem fisk og kød
-        protein = random.choice(fisk)
-    else:
-        protein = random.choice(kod)
-    kryderier = random.choice(kryderier)
+def generate_recipe(vegetables, meats, fishes, spices):
+    vegetable = random.choice(vegetables)
+    protein = choose_protein()
+    spice = random.choice(spices)
 
-    opskrift = f"Opskrift: Stegt {protein} med {gronsag} og krydret med {kryderier}."
-    return opskrift
+    recipe = f"Opskrift: Stegt {protein} med {vegetable} og krydret med {spice}."
+    return recipe
+
+def choose_protein():
+    # Få ugedagen som et tal (mandag=0, tirsdag=1, ..., søndag=6)
+    weekday = datetime.datetime.today().weekday()
+    
+    # Hvis det er mandag til torsdag, vælg kød
+    if weekday < 4:  # Mandag til torsdag
+        return random.choice(meats)
+    else:  # Fredag til søndag
+        return random.choice(fishes)
 
 # Liste med grøntsager
-gronsag = ["broccoli", "gulerødder", "spinat", "tomater", "aubergine", "peberfrugt"]
+vegetables = ["broccoli", "gulerødder", "spinat", "tomater", "aubergine", "peberfrugt"]
 
 # Liste med kød
-kod = ["kylling", "oksekød", "svinekød", "lam"]
+meats = ["kylling", "oksekød", "svinekød", "lam"]
 
 # Liste med fisk
-fisk = ["laks", "tun", "torsk", "rødspætte"]
+fishes = ["laks", "tun", "torsk", "rødspætte"]
 
 # Liste med krydderier
-kryderier = ["oregano", "timian", "rosmarin", "karry", "kanel", "spidskommen"]
+spices = ["oregano", "timian", "rosmarin", "karry", "kanel", "spidskommen"]
 
 # Generer og udskriv opskrift
-opskrift = lav_opskrift(gronsag, kod, fisk, kryderier)
-print(opskrift)
+recipe = generate_recipe(vegetables, meats, fishes, spices)
+print(recipe)
