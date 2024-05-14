584edec7ea3a87811407e7277c3a65b37fa812c7
MelCop <charliedem20@gmail.com>
2024-05-14 10:46:02 +0200

    a

---
+++ scanIP.py
@@ -0,0 +1,13 @@
+from scapy.all import IP, ICMP, sr1
+
+def ping(host):
+    packet = IP(dst=host)/ICMP()
+    response = sr1(packet, timeout=2, verbose=0)
+    if response:
+        return f"{host} is online"
+    else:
+        return f"{host} is offline"
+    
+host_to_scan = "dr.dk"
+result = ping(host_to_scan)
+print(result)
