5955294a855fe93e89f7a049157d0fb09179392f
MelCop <charliedem20@gmail.com>
2024-05-31 09:41:57 +0200

    a

--- pandasseries.py
+++ pandasseries.py
@@ -4,9 +4,10 @@ series = pd.Series(l)
 series[1:5] = 70
 print(series)
 
-d= {1:"Mango",2:"Apple",3:"Guava",4:"Kiwi"}
+d = {1:"Mango",2:"Apple",3:"Guava",4:"Kiwi"}
 e = {1:"esb", 2:"rpi", 3:"arduino"}
 seriesd = pd.Series(d)
 seriese = pd.Series(e)
+print(d.get(2))
 resultlist = seriesd[1:4] + seriese[1:3]
 print(resultlist)
