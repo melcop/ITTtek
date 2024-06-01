3011824f753f84d0ac1cbed587672f0717738d20
MelCop <charliedem20@gmail.com>
2024-06-01 22:06:15 +0200

    a

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ list4 = [22, 1, 9, 2]
 print(list3 < list4)
 
 # Den her del forstår jeg ikke og det vil jeg kigge nærmere 
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5, 6, 7]
 result = my_list[1:4:2]
 print(result)
 
