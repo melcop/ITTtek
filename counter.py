3e89caec28ad3ca383658205a4c1b0d21f8ae4d4
melcop <charliedem20@gmail.com>
2024-06-06 13:41:40 +0200

    a

---
+++ counter.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        seconds -= 1
+
+    print("Tid er gået!")
+
+# Brug nedtællingsuret ved at angive antal sekunder
+countdown_timer(10)  # Nedtælling i 10 sekunder
