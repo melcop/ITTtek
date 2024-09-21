53e489a95934cceba72c2c964179a4560f068856
melcop <charliedem20@gmail.com>
2024-09-21 13:48:36 +0200

    a

--- cont.py
+++ cont.py
@@ -1,5 +1,7 @@
 a = 1
 b = 2
+c = 4
+d = 3
 print (a+b)
 
 print(a//b)
@@ -15,3 +17,10 @@ print(b-a+2)
 print(a*a*b)
 
 print("echo")
+
+
+list = [a, d, b, c]
+print(list)
+list.sort()
+print("------------")
+print(list)
