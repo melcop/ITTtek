72c547ebdb1e13623386071ba53b701c9646f378
melcop <charliedem20@gmail.com>
2024-10-05 13:13:54 +0200

    a

--- book.py
+++ book.py
@@ -5,7 +5,7 @@ import random
 turtle.bgcolor('black')
 turtle.speed('fast')
 turtle.pensize(4)
-sleep(10)
+
 print("need")
 sleep(5)
 print("speed")
@@ -13,3 +13,5 @@ print("speed")
 # Hacks and tweaks s.105
 pen_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 print(random.choice(pen_colors))
+
+print(pen_colors.count('red'))
