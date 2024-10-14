734ee964611603ccbaf9e5e7815d06f1692c4b8d
melcop <charliedem20@gmail.com>
2024-10-14 15:49:44 +0200

    a

--- catgame.py
+++ catgame.py
@@ -2,7 +2,7 @@ import random
 
 lives = 9
 
-words = ['pizza', 'fairy', 'shoes', 'tent', 'teeth', 'shirt', 'otter', 'plane', 'plate']
+words = ['pizza', 'fairy', 'shoes', 'tent', 'teeth', 'shirt', 'shirt','otter', 'plane', 'plate']
 secret_word = random.choice(words)
 print(secret_word)
 print("--------------")
