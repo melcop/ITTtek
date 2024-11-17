2f95c1e10bb9b73208c918013b40cb6d717247ae
melcop <charliedem20@gmail.com>
2024-11-17 14:21:47 +0100

    a

---
+++ vejr.py
@@ -0,0 +1,8 @@
+vejr = input('Hvordan er vejret i dag? (regn/sne/sol)')
+
+if vejr == 'regn':
+    print('Husk paraply!')
+elif vejr == 'sne':
+    print('Husk handsker!')
+else:
+    print('Husk solbriller!')
