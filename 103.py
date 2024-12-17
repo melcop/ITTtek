59d2532573cde4f26b68b15069ca16c1d4ab1997
melcop <charliedem20@gmail.com>
2024-12-17 16:50:09 +0100

    a

--- 103.py
+++ 103.py
@@ -66,7 +66,7 @@ if num == 1:
     print(num, "is not a prime number")
 elif num > 1:
     # check for factors
-    for i in range(2, num):
+    for i in range(3, num):
         if (num % i) == 0:
             # if factor is found, set flag to True
             flag = True
