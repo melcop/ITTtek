aeaf6fc67ba09387c208ce91256b121ef0f4213c
melcop <charliedem20@gmail.com>
2025-02-06 08:49:40 +0100

    a

--- sletdatafracomp.py
+++ sletdatafracomp.py
@@ -2,7 +2,7 @@ import sqlite3
 conn = sqlite3.connect('testDB.db')
 try:
     cur = conn.cursor()
-    cur.execute("DELETE FROM Company WHERE Name LIKE ?", ('da%',))
+    cur.execute("DELETE FROM Company WHERE Name LIKE ?", ('d%',))
     conn.commit()
 except sqlite3.Error as e:
     print(f'Unable to delete Name from database ! {e}')
